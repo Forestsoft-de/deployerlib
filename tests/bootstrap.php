55b3b65309567ee5791a34302a42af41ee2f614e
Sebastian Foerster <foerster@forestsoft.de>
2019-02-16 21:44:15 +0100

    Make tests working for host configuration

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,7 @@
 
 use Deployer\Console\Application;
 use Deployer\Deployer;
+use function Deployer\inventory;
 use Deployer\Task\Context;
 
 include_once dirname(__DIR__) . '/vendor/autoload.php';
@@ -16,11 +17,11 @@ $deployer->init();
 $input = new \Symfony\Component\Console\Input\ArgvInput();
 $output = new \Symfony\Component\Console\Output\BufferedOutput();
 
-$localhost = new \Deployer\Host\Localhost("dev.local");
-$localhost->set("foo", "bar");
-Context::push(new Context($localhost, $input, $output));
-
 copy(FIXTURES . "/hosts.yml", __DIR__ . "/hosts.yml");
+inventory(__DIR__ . "/hosts.yml");
+
+$localhost = Deployer::get()->hostSelector->getByHostnames("dev.local");
+Context::push(new Context($localhost[0], $input, $output));
 
 
 
